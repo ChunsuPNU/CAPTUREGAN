70369cbd2dcaab0f0e54ab9d305cf82f4bad99d6
Chunsu <cspark5484@gmail.com>
2025-03-04 12:16:56 +0900

    Commit init

--- model.py
+++ model.py
@@ -121,3 +121,146 @@ class Discriminator_cycle(nn.Module):
         x = self.enc5(x)
 
         return torch.sigmoid(x)
+
+
+# U-Net: Convolutional Networks for Biomedical Image Segmentation
+# https://arxiv.org/abs/1505.04597
+class UNet(nn.Module):
+    def __init__(self, input_nc, output_nc, nker=64, learning_type="plain", norm="bnorm"):
+        super(UNet, self).__init__()
+
+        self.learning_type = learning_type
+
+        # Contracting path
+        self.enc1_1 = CBR2d(input_nc=input_nc, output_nc=1 * nker, norm=norm)
+        self.enc1_2 = CBR2d(input_nc=1 * nker, output_nc=1 * nker, norm=norm)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc2_1 = CBR2d(input_nc=nker, output_nc=2 * nker, norm=norm)
+        self.enc2_2 = CBR2d(input_nc=2 * nker, output_nc=2 * nker, norm=norm)
+
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc3_1 = CBR2d(input_nc=2 * nker, output_nc=4 * nker, norm=norm)
+        self.enc3_2 = CBR2d(input_nc=4 * nker, output_nc=4 * nker, norm=norm)
+
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc4_1 = CBR2d(input_nc=4 * nker, output_nc=8 * nker, norm=norm)
+        self.enc4_2 = CBR2d(input_nc=8 * nker, output_nc=8 * nker, norm=norm)
+
+        self.pool4 = nn.MaxPool2d(kernel_size=2)
+
+        self.enc5_1 = CBR2d(input_nc=8 * nker, output_nc=16 * nker, norm=norm)
+
+        # Expansive path
+        self.dec5_1 = CBR2d(input_nc=16 * nker, output_nc=8 * nker, norm=norm)
+
+        self.unpool4 = nn.ConvTranspose2d(in_channels=8 * nker, out_channels=8 * nker,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec4_2 = CBR2d(input_nc=2 * 8 * nker, output_nc=8 * nker, norm=norm)
+        self.dec4_1 = CBR2d(input_nc=8 * nker, output_nc=4 * nker, norm=norm)
+
+        self.unpool3 = nn.ConvTranspose2d(in_channels=4 * nker, out_channels=4 * nker,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec3_2 = CBR2d(input_nc=2 * 4 * nker, output_nc=4 * nker, norm=norm)
+        self.dec3_1 = CBR2d(input_nc=4 * nker, output_nc=2 * nker, norm=norm)
+
+        self.unpool2 = nn.ConvTranspose2d(in_channels=2 * nker, out_channels=2 * nker,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec2_2 = CBR2d(input_nc=2 * 2 * nker, output_nc=2 * nker, norm=norm)
+        self.dec2_1 = CBR2d(input_nc=2 * nker, output_nc=1 * nker, norm=norm)
+
+        self.unpool1 = nn.ConvTranspose2d(in_channels=1 * nker, out_channels=1 * nker,
+                                          kernel_size=2, stride=2, padding=0, bias=True)
+
+        self.dec1_2 = CBR2d(input_nc=2 * 1 * nker, output_nc=1 * nker, norm=norm)
+        self.dec1_1 = CBR2d(input_nc=1 * nker, output_nc=1 * nker, norm=norm)
+
+        self.fc = nn.Conv2d(in_channels=1 * nker, out_channels=output_nc, kernel_size=1, stride=1, padding=0, bias=True)
+
+    def forward(self, x):
+        enc1_1 = self.enc1_1(x)
+        enc1_2 = self.enc1_2(enc1_1)
+        pool1 = self.pool1(enc1_2)
+
+        enc2_1 = self.enc2_1(pool1)
+        enc2_2 = self.enc2_2(enc2_1)
+        pool2 = self.pool2(enc2_2)
+
+        enc3_1 = self.enc3_1(pool2)
+        enc3_2 = self.enc3_2(enc3_1)
+        pool3 = self.pool3(enc3_2)
+
+        enc4_1 = self.enc4_1(pool3)
+        enc4_2 = self.enc4_2(enc4_1)
+        pool4 = self.pool4(enc4_2)
+
+        enc5_1 = self.enc5_1(pool4)
+
+        dec5_1 = self.dec5_1(enc5_1)
+
+        unpool4 = self.unpool4(dec5_1)
+        cat4 = torch.cat((unpool4, enc4_2), dim=1)
+        dec4_2 = self.dec4_2(cat4)
+        dec4_1 = self.dec4_1(dec4_2)
+
+        unpool3 = self.unpool3(dec4_1)
+        cat3 = torch.cat((unpool3, enc3_2), dim=1)
+        dec3_2 = self.dec3_2(cat3)
+        dec3_1 = self.dec3_1(dec3_2)
+
+        unpool2 = self.unpool2(dec3_1)
+        cat2 = torch.cat((unpool2, enc2_2), dim=1)
+        dec2_2 = self.dec2_2(cat2)
+        dec2_1 = self.dec2_1(dec2_2)
+
+        unpool1 = self.unpool1(dec2_1)
+        cat1 = torch.cat((unpool1, enc1_2), dim=1)
+        dec1_2 = self.dec1_2(cat1)
+        dec1_1 = self.dec1_1(dec1_2)
+
+        if self.learning_type == "plain":
+            x = self.fc(dec1_1)
+        elif self.learning_type == "residual":
+            x = x + self.fc(dec1_1)
+
+        return x
+
+
+# Deep Residual Learning for Image Recognition
+# https://arxiv.org/abs/1512.03385
+class ResNet(nn.Module):
+    def __init__(self, input_nc, output_nc, nker=64, learning_type="plain", norm="bnorm", nblk=16):
+        super(ResNet, self).__init__()
+
+        self.learning_type = learning_type
+
+        self.enc = CBR2d(input_nc, nker, kernel_size=3, stride=1, padding=1, bias=True, norm=None, relu=0.0)
+
+        res = []
+        for i in range(nblk):
+            res += [ResBlock(nker, nker, kernel_size=3, stride=1, padding=1, bias=True, norm=norm, relu=0.0)]
+        self.res = nn.Sequential(*res)
+
+        self.dec = CBR2d(nker, nker, kernel_size=3, stride=1, padding=1, bias=True, norm=norm, relu=0.0)
+
+        self.fc = CBR2d(nker, output_nc, kernel_size=1, stride=1, padding=0, bias=True, norm=None, relu=None)
+
+    def forward(self, x):
+        x0 = x
+
+        x = self.enc(x)
+        x = self.res(x)
+        x = self.dec(x)
+
+        if self.learning_type == "plain":
+            x = self.fc(x)
+        elif self.learning_type == "residual":
+            x = x0 + self.fc(x)
+
+        return x
